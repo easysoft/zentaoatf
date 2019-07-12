b643925e09ace5445037da57cb13dcbc65568e2b
Aaron Chen <462826@qq.com>
2019-07-12 09:50:29 +0800

    use struct to save the logs

--- src/biz/match.go
+++ src/biz/match.go
@@ -8,21 +8,20 @@ import (
 
 func MatchString(expect string, actual string, langType string) bool {
 	if langType == misc.PHP.String() {
-		// $wantedReg = preg_quote($wanted, '/');
-		expect = strings.Replace(expect, "%s", `.+?`, -1)
-		expect = strings.Replace(expect, "%i", `[+\-]?[0-9]+`, -1)
-		expect = strings.Replace(expect, "%d", `[0-9]+`, -1)
-		expect = strings.Replace(expect, "%x", `[0-9a-fA-F]+`, -1)
-		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1)
-		expect = strings.Replace(expect, "%c", ".", -1)
+		expect = strings.Replace(expect, "%s", `.+?`, -1)                                  // 字符串
+		expect = strings.Replace(expect, "%i", `[+\-]?[0-9]+`, -1)                         // 十进制数字，可有符号
+		expect = strings.Replace(expect, "%d", `[0-9]+`, -1)                               // 十进制数字，无符号
+		expect = strings.Replace(expect, "%x", `[0-9a-fA-F]+`, -1)                         // 十六进制数字
+		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1) // 十进制浮点数
+		expect = strings.Replace(expect, "%c", ".", -1)                                    // 单个字符
 	} else if langType == misc.GO.String() {
-		expect = strings.Replace(expect, "%s", `.+?`, -1)
-		expect = strings.Replace(expect, "%v", `.+?`, -1)
-		expect = strings.Replace(expect, "%d", `[0-9]+`, -1)
-		expect = strings.Replace(expect, "%t", `(true|false)+`, -1)
-		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1)
+		expect = strings.Replace(expect, "%s", `.+?`, -1)                                  // 字符串
+		expect = strings.Replace(expect, "%v", `.+?`, -1)                                  // 字符串
+		expect = strings.Replace(expect, "%d", `[0-9]+`, -1)                               // 十进制数字，无符号
+		expect = strings.Replace(expect, "%t", `(true|false)+`, -1)                        // true或false
+		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1) // 浮点数
 	}
 
-	pass, _ := regexp.MatchString(expect, actual)
+	pass, _ := regexp.MatchString("^"+expect+"$", actual)
 	return pass
 }
