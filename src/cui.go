54445ad11c3dd6ec021e476d4aa2adfe366ca77e
Aaron Chen <462826@qq.com>
2019-07-24 12:23:47 +0800

    impost project

--- src/cui.go
+++ src/cui.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	"github.com/jroimartin/gocui"
+	"log"
 )
 
 func layout(g *gocui.Gui) error {
@@ -15,10 +14,10 @@ func layout(g *gocui.Gui) error {
 			return err
 		}
 
+		v.Frame = true
 		v.Highlight = true
 		v.SelBgColor = gocui.ColorGreen
 		v.SelFgColor = gocui.ColorBlack
-		fmt.Fprintln(v, "New Project")
 		fmt.Fprintln(v, "New From Zentao")
 		fmt.Fprintln(v, "Switch Project")
 	}
@@ -27,9 +26,9 @@ func layout(g *gocui.Gui) error {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-
 		v.Autoscroll = true
 	}
+
 	if v, err := g.SetView("cmdline", int(0.2*float32(maxX)), maxY-5, maxX-1, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
@@ -69,12 +68,12 @@ func keybindings(g *gocui.Gui) error {
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("side", gocui.MouseLeft, gocui.ModNone, showMsg); err != nil {
+	if err := g.SetKeybinding("side", gocui.MouseLeft, gocui.ModNone, newProjectFormZentao); err != nil {
 		return err
 	}
-	//if err := g.SetKeybinding("cmdline", gocui.MouseLeft, gocui.ModNone, setEdit); err != nil {
-	//	return err
-	//}
+	if err := g.SetKeybinding("cmdline", gocui.MouseLeft, gocui.ModNone, setEdit); err != nil {
+		return err
+	}
 	if err := g.SetKeybinding("msg", gocui.MouseLeft, gocui.ModNone, delMsg); err != nil {
 		return err
 	}
@@ -97,19 +96,6 @@ func showMsg(g *gocui.Gui, v *gocui.View) error {
 	mainView, err := g.View("main")
 	fmt.Fprintln(mainView, l)
 
-	if _, err := g.SetCurrentView("cmdline"); err != nil {
-		return err
-	}
-	v.SetCursor(0, 0)
-	v.SetOrigin(0, 0)
-
-	//maxX, maxY := g.Size()
-	//if v, err := g.SetView("msg", maxX/2-10, maxY/2, maxX/2+10, maxY/2+2); err != nil {
-	//	if err != gocui.ErrUnknownView {
-	//		return err
-	//	}
-	//	fmt.Fprintln(v, l)
-	//}
 	return nil
 }
 
@@ -122,6 +108,76 @@ func delMsg(g *gocui.Gui, v *gocui.View) error {
 }
 
 func setEdit(g *gocui.Gui, v *gocui.View) error {
+	if _, err := g.SetCurrentView("cmdline"); err != nil {
+		return err
+	}
+
+	//v.SetOrigin(0, 0)
+	//v.SetCursor(0, 0)
+
+	v.SetCursor(0, 0)
+	v.Clear()
+
+	return nil
+}
+
+func newProjectFormZentao(g *gocui.Gui, slide *gocui.View) error {
+	slideView, _ := g.View("side")
+	slideX, _ := slideView.Size()
+
+	//mainView, _ := g.View("main")
+	//mainX, mainY := mainView.Size()
+
+	labelWidth := 10
+	inputWidth := 30
+	space := 2
+	left := slideX + 3
+	right := left + labelWidth
+
+	if v, err := g.SetView("productLabel", left, 1, right, 3); err != nil {
+		v.Frame = false
+		fmt.Fprintln(v, "ProjectId")
+	}
+
+	left = right + space
+	right = left + inputWidth
+	if v, err := g.SetView("productInput", left, 1, right, 3); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Editable = true
+		v.Wrap = true
+
+		if _, err := g.SetCurrentView("productInput"); err != nil {
+			return err
+		}
+	}
+
+	left = right + space
+	right = left + 3
+	if v, err := g.SetView("or", left, 1, right, 3); err != nil {
+		v.Frame = false
+		fmt.Fprintln(v, "or")
+	}
+
+	left = right + space
+	right = left + (labelWidth - 3)
+	if v, err := g.SetView("planLabel", left, 1, right, 3); err != nil {
+		v.Frame = false
+		fmt.Fprintln(v, "PlanId")
+	}
+
+	left = right + space
+	right = left + inputWidth
+	if v, err := g.SetView("planInput", left, 1, right, 3); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Editable = true
+		v.Wrap = true
+	}
 
 	return nil
 }
