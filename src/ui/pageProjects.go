4e66720ca454efacbefd153e1ea564ce2bf05843
Aaron Chen <462826@qq.com>
2019-07-30 11:11:42 +0800

    switch project by clicking project history item

--- src/ui/pageProjects.go
+++ src/ui/pageProjects.go
@@ -1,9 +1,12 @@
 package ui
 
 import (
+	"fmt"
+	"github.com/easysoft/zentaoatf/src/action"
 	"github.com/easysoft/zentaoatf/src/model"
 	"github.com/easysoft/zentaoatf/src/utils"
 	"github.com/jroimartin/gocui"
+	"time"
 )
 
 var CurrProjectName string
@@ -11,12 +14,12 @@ var projectHistoriess []model.WorkHistory
 
 func InitProjectsPage(g *gocui.Gui) error {
 	his := utils.Prefer.WorkHistories[0]
-	name, _ := getProjectInfo(his)
+	name, _, _ := getProjectInfo(his)
 	CurrProjectName = name
 
 	y := 2
 	for _, his := range utils.Prefer.WorkHistories {
-		name, label := getProjectInfo(his)
+		name, label, _ := getProjectInfo(his)
 
 		hisView := NewLabelWidgetAutoWidth(g, name, 0, y, label)
 		ViewMap["projects"] = append(ViewMap["projects"], hisView.Name())
@@ -30,7 +33,7 @@ func InitProjectsPage(g *gocui.Gui) error {
 
 func keybindingProjectsButton(g *gocui.Gui) error {
 	for _, his := range utils.Prefer.WorkHistories {
-		name, _ := getProjectInfo(his)
+		name, _, _ := getProjectInfo(his)
 		if err := g.SetKeybinding(name, gocui.MouseLeft, gocui.ModNone, toggleProjectsButton); err != nil {
 			return err
 		}
@@ -48,15 +51,18 @@ func toggleProjectsButton(g *gocui.Gui, v *gocui.View) error {
 
 func SelectProjectsButton(g *gocui.Gui) {
 	for _, his := range utils.Prefer.WorkHistories {
-		name, _ := getProjectInfo(his)
+		name, _, path := getProjectInfo(his)
 
 		v, err := g.View(name)
-
 		if err == nil {
 			if v.Name() == CurrProjectName {
 				v.Highlight = true
 				v.SelBgColor = gocui.ColorWhite
 				v.SelFgColor = gocui.ColorBlack
+
+				action.SwitchWorkDir(path)
+				utils.PrintToCmd(g, fmt.Sprintf("success to switch project to %s at %s",
+					path, utils.DateTimeStr(time.Now())))
 			} else {
 				v.Highlight = false
 				v.SelBgColor = gocui.ColorBlack
@@ -66,10 +72,12 @@ func SelectProjectsButton(g *gocui.Gui) {
 	}
 }
 
-func getProjectInfo(his model.WorkHistory) (string, string) {
+func getProjectInfo(his model.WorkHistory) (string, string, string) {
 	var name string
 	var label string
+	var path string
 
+	path = his.ProjectPath
 	if his.EntityType != "" {
 		name = his.EntityType + "-" + his.EntityVal
 		label = his.ProjectName
@@ -78,7 +86,7 @@ func getProjectInfo(his model.WorkHistory) (string, string) {
 		label = utils.PathSomple(his.ProjectPath)
 	}
 
-	return name, label
+	return name, label, path
 }
 
 func init() {
