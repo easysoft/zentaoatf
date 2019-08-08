ab26078a78e02ae7c3461597d3e0e10857e38ec3
Aaron Chen <462826@qq.com>
2019-08-08 13:05:55 +0800

    Revert "test and fix on win7"
    This reverts commit 50097e26d079e5c9d9d2b6ea561cdad510c4af18.
    
    # Conflicts:
    #	src/test.go

--- src/test.go
+++ src/test.go
@@ -20,7 +20,10 @@ func main() {
  
 dd`
 
-	myExp := regexp.MustCompile("(?m:^\\s(?:PASS|FAIL) scripts/tc-200.py\n([\\s\\S]*?)((^\\s(PASS|FAIL))|\\z))")
+	str := "(?m:^\\s" + "FAIL\\sscripts/tc-200.py" + "\\n([\\s\\S]*?)((^\\s(PASS|FAIL))|\\z))"
+	// myExp := regexp.MustCompile("(?m:^\\s(?:PASS|FAIL) scripts/tc-200.py\n([\\s\\S]*?)((^\\s(PASS|FAIL))|\\z))")
+
+	myExp := regexp.MustCompile(str)
 	arr := myExp.FindStringSubmatch(text)
 
 	fmt.Println(arr[1])
