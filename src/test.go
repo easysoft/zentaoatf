e82714a0f7bf6baf81c8c203c45e90b6a8904b71
Aaron Chen <462826@qq.com>
2019-08-08 13:19:21 +0800

    fix regx issue in windows

--- src/test.go
+++ src/test.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/easysoft/zentaoatf/src/utils"
 	"regexp"
 )
 
 func main() {
-	text := ` df 
- FAIL scripts\tc-200.py
-  Step1: FAIL   @step2010 第4次尝试登录
-    Checkpoint1: FAIL
-      Expect Result CODE: @step2010期望结果, 可以有多行
-      Actual Result N/A
-
-  Step2: FAIL   @step2104 再输入1次正确的密码
-    Checkpoint1: FAIL
-      Expect Result CODE: @step2104期望结果, 可以有多行
-      Actual Result N/A
- 
-dd`
+	text := utils.ReadFile("result.txt")
 
 	str := "(?m:^\\s" + "FAIL\\sscripts\\\\tc-200.py" + "\\n([\\s\\S]*?)((^\\s(PASS|FAIL))|\\z))"
 	// myExp := regexp.MustCompile("(?m:^\\s(?:PASS|FAIL) scripts\\tc-200.py\n([\\s\\S]*?)((^\\s(PASS|FAIL))|\\z))")
