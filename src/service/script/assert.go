2a4fc8cd46390515552f4caf5fd29690d1fdb194
Aaron Chen <462826@qq.com>
2019-08-16 15:42:15 +0800

    add textarea input for bug steps for user edit

--- src/service/script/assert.go
+++ src/service/script/assert.go
@@ -1,10 +1,16 @@
 package scriptService
 
 import (
+	"fmt"
+	commonUtils "github.com/easysoft/zentaoatf/src/utils/common"
 	config2 "github.com/easysoft/zentaoatf/src/utils/config"
 	constant "github.com/easysoft/zentaoatf/src/utils/const"
 	"github.com/easysoft/zentaoatf/src/utils/file"
 	"github.com/easysoft/zentaoatf/src/utils/vari"
+	"io/ioutil"
+	"path"
+	"regexp"
+	"strings"
 )
 
 func LoadAssetFiles() ([]string, []string) {
@@ -18,4 +24,44 @@ func LoadAssetFiles() ([]string, []string) {
 	fileUtils.GetAllFiles(vari.Prefer.WorkDir+constant.ScriptDir, "suite", &suitesFiles)
 
 	return caseFiles, suitesFiles
+}
+
+func LoadTestResults(assert string) []string {
+	ret := make([]string, 0)
+
+	dir := vari.Prefer.WorkDir + constant.LogDir
+
+	mode, name := GetRunModeAndName(assert)
+	reg := fmt.Sprintf("%s-%s-(.+)", mode, name)
+	myExp := regexp.MustCompile(reg)
+
+	files, _ := ioutil.ReadDir(dir)
+	for _, fi := range files {
+		if fi.IsDir() {
+			arr := myExp.FindStringSubmatch(fi.Name())
+			if len(arr) > 1 {
+				ret = append(ret, arr[1])
+			}
+		}
+	}
+
+	return ret
+}
+
+func GetRunModeAndName(assert string) (string, string) {
+	ext := path.Ext(assert)
+	name := strings.Replace(commonUtils.Base(assert), ext, "", -1)
+
+	var mode string
+	if ext == ".suite" {
+		mode = "suite"
+	} else {
+		mode = "script"
+	}
+
+	return mode, name
+}
+
+func GetLogFolder(mode string, name string, date string) string {
+	return fmt.Sprintf("%s-%s-%s", mode, name, date)
 }
