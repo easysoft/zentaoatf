04b4bed36b8bf8a6a31fb79fc4390614974d29b3
Aaron Chen <462826@qq.com>
2019-08-22 17:03:12 +0800

    sample script

--- scripts-demo/tc-1.sh
+++ scripts-demo/tc-1.sh
@@ -3,13 +3,18 @@
 caseId:         1
 caseIdInTask:   0
 taskId:         0
-title:          Demo测试用例
+title:          测试服务器响应时间
 steps:          @开头的为含验证点的步骤
-   step1           ping服务器zentao.com
-   @step2           验证有返回，且响应时间小于300ms
+   step1           执行ping命令，向zentao.com服务器发送ICMP请求
+   step2           获取返回消息，截取响应时间字段，转换成整数
+   step3           判断响应时间，如果超过300ms，则放弃后续请求并返回
+                               如果小于300ms，重复以上步骤累计3次
+   @step4          验证最后返回的响应时间，如果小于300ms，打印"work"
+                                        如果大于300ms，打印"timeout"
+                                        如果为空，打印unknown表明为未到服务器
 
 expects:
-# @step2
+# @step4
 work
 
 readme:
