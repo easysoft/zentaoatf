6655194c3ea13c085a146189b2a653482872287e
Aaron Chen <462826@qq.com>
2019-08-23 16:37:51 +0800

    test bat scripts

--- scripts-demo/tc-1.bat
+++ scripts-demo/tc-1.bat
@@ -1,3 +1,30 @@
+goto s
+<<<TC
+
+caseId:         1
+caseIdInTask:   0
+taskId:         0
+title:          Test site response time
+steps:          steps that begin with @ are checkpoints
+   step1           type "ping zentao.com" to send ICMP request
+   step2           get response time from output
+   step3           if time > 300ms, break the cycle
+                      time < 300ms, continue
+   @step4          check the last response time，if time < 300ms，print "work"
+                                                    time > 300ms，print "timeout"
+
+expects:
+# @step4
+work
+
+readme:
+- Print '#' in test log to match up with the ones in expects section
+- Write test scripts to replace the lines begin with 'CODE'
+- More examples, pls refer to https://github.com/easysoft/zentaoatf/tree/master/xdoc/sample
+
+TC;
+:s
+
 @echo off
 Setlocal enabledelayedexpansion
 
