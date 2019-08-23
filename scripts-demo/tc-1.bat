fbef55e837574def412c13bb4c85611e1f926e15
Aaron Chen <462826@qq.com>
2019-08-23 16:23:12 +0800

    test bat scripts

--- scripts-demo/tc-1.bat
+++ scripts-demo/tc-1.bat
@@ -1,31 +1,3 @@
-goto s
-<<<TC
-
-caseId:         1
-caseIdInTask:   0
-taskId:         0
-title:          测试服务器响应时间
-steps:          @开头的为含验证点的步骤
-   step1           执行ping命令，向zentao.com服务器发送ICMP请求
-   step2           获取返回消息，截取响应时间字段，转换成整数
-   step3           判断响应时间，如果超过300ms，则放弃后续请求并返回
-                               如果小于300ms，重复以上步骤累计3次
-   @step4          验证最后返回的响应时间，如果小于300ms，打印"work"
-                                        如果大于300ms，打印"timeout"
-                                        如果为空，打印unknown表明为未到服务器
-
-expects:
-# @step4
-work
-
-readme:
-- Logs of test scripts，must expects章节中#号标注的验证点需保持一致对应
-- 脚本中CODE打头的注释需用代码替换
-- 参考样例https://github.com/easysoft/zentaoatf/tree/master/xdoc/sample
-
-TC;
-:s
-
 @echo off
 Setlocal enabledelayedexpansion
 
