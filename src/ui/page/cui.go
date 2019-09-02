fea7f75bc5352857e1145c748a5b95b566237588
Aaron Chen <462826@qq.com>
2019-09-02 18:11:54 +0800

    test on win10

--- src/ui/page/cui.go
+++ src/ui/page/cui.go
@@ -28,7 +28,7 @@ func CuiReportBug(dir string, id string) error {
 
 	InitMainPage()
 
-	InitReportBugPage(dir, id)
+	//InitReportBugPage(dir, id)
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
