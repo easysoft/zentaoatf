c421c4c53414d3024ac0a2827c6c4d683f8932cf
Aaron Chen <462826@qq.com>
2019-09-11 15:20:52 +0800

    add python string format support

--- src/utils/string/match.go
+++ src/utils/string/match.go
@@ -14,12 +14,6 @@ func MatchString(expect string, actual string, langType string) bool {
 		expect = strings.Replace(expect, "%x", `[0-9a-fA-F]+`, -1)                         // 十六进制数字
 		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1) // 十进制浮点数
 		expect = strings.Replace(expect, "%c", ".", -1)                                    // 单个字符
-	} else if langType == constant.GO.String() {
-		expect = strings.Replace(expect, "%s", `.+?`, -1)                                  // 字符串
-		expect = strings.Replace(expect, "%v", `.+?`, -1)                                  // 字符串
-		expect = strings.Replace(expect, "%d", `[0-9]+`, -1)                               // 十进制数字，无符号
-		expect = strings.Replace(expect, "%t", `(true|false)+`, -1)                        // true或false
-		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1) // 浮点数
 	} else if langType == constant.PYTHON.String() {
 		expect = strings.Replace(expect, "%s", `.+?`, -1)                                  // 字符串
 		expect = strings.Replace(expect, "%d", `[+\-]?[0-9]+`, -1)                         // 十进制数字，可有符号
@@ -27,6 +21,12 @@ func MatchString(expect string, actual string, langType string) bool {
 		expect = strings.Replace(expect, "%x", `[0-9a-fA-F]+`, -1)                         // 十六进制数字
 		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1) // 十进制浮点数
 		expect = strings.Replace(expect, "%c", ".", -1)                                    // 单个字符
+	} else if langType == constant.GO.String() {
+		expect = strings.Replace(expect, "%s", `.+?`, -1)                                  // 字符串
+		expect = strings.Replace(expect, "%v", `.+?`, -1)                                  // 字符串
+		expect = strings.Replace(expect, "%d", `[0-9]+`, -1)                               // 十进制数字，无符号
+		expect = strings.Replace(expect, "%t", `(true|false)+`, -1)                        // true或false
+		expect = strings.Replace(expect, "%f", `[+\-]?\.?[0-9]+\.?[0-9]*(E-?[0-9]+)?`, -1) // 浮点数
 	}
 
 	pass, _ := regexp.MatchString("^"+expect+"$", actual)
