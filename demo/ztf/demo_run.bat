8c4f53fa804e7e39cb0139cfb2cd1721b458adc1
aaron <462826@qq.com>
2019-10-10 10:25:37 +0800

    complete the demo test

--- demo/ztf/demo_run.bat
+++ demo/ztf/demo_run.bat
@@ -2,4 +2,4 @@
 
 rm -rf log/*
 ztf.exe run demo/lang demo/sample
-ztf.exe run demo/ztf/demo_test.bat
+ztf.exe run demo/ztf/demo_check.bat
--- demo/ztf/demo_run.sh
+++ demo/ztf/demo_run.sh
@@ -2,4 +2,4 @@
 
 rm -rf log/*
 ztf run demo/lang demo/sample
-ztf run demo/ztf/demo_test.sh
+ztf run demo/ztf/demo_check.sh
