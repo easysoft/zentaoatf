5e40fe7893f487467192c483121d921aad46f453
aaron <462826@qq.com>
2019-10-10 09:35:45 +0800

    use demo script to test

--- demo/ztf/demo_test.bat
+++ demo/ztf/demo_test.bat
@@ -6,41 +6,44 @@ cid=0
 pid=0
 
 [group]
-javascript/1_string_match.js             >> Pass
-javascript/2_webpage_extract.js          >> Pass
-javascript/3_http_inteface_call.js       >> Pass
-lua/1_string_match.lua                   >> Pass
-lua/2_webpage_extract.lua                >> Pass
-lua/3_http_inteface_call.lua             >> Pass
-perl/1_string_match.pl                   >> Pass
-perl/2_webpage_extract.pl                >> Pass
-perl/3_http_inteface_call.pl             >> Pass
-php/1_string_match.php                   >> Pass
-php/2_webpage_extract.php                >> Pass
-php/3_http_inteface_call.php             >> Pass
-python/1_string_match.py                 >> Pass
-python/2_webpage_extract.py              >> Pass
-python/3_http_inteface_call.py           >> Pass
-ruby/1_string_match.rb                   >> Pass
-ruby/2_webpage_extract.rb                >> Pass
-ruby/3_http_inteface_call.rb             >> Pass
-shell/1_string_match.sh                  >> Pass
-shell/2_webpage_extract.sh               >> Pass
-shell/3_http_inteface_call.sh            >> Pass
-tcl/1_string_match.tl                    >> Pass
-tcl/2_webpage_extract.tl                 >> Pass
-tcl/3_http_inteface_call.tl              >> Pass
-sample/1_simple.php                      >> Pass
-sample/2_with_group.php                  >> Pass
-sample/3_step_multi_lines.php            >> Pass
-sample/4_expect_saved_to_file.php        >> Pass
-sample/5_expect_with_express.php         >> Pass
-sample/6_expect_with_regx.php            >> Pass
-sample/7_expect_with_format_string.php   >> Pass
-sample/8_misc.php                        >> Pass
-sample/9_skip.php                        >> Skip
+  1. bat/1_string_match.sh >> Pass
+  2. bat/2_webpage_extract.sh >> Pass
+  3. bat/3_http_inteface_call.sh >> Pass
+  4. javascript/1_string_match.js >> Pass
+  5. javascript/2_webpage_extract.js >> Pass
+  6. javascript/3_http_inteface_call.js >> Pass
+  7. lua/1_string_match.lua >> Pass
+  8. lua/2_webpage_extract.lua >> Pass
+  9. lua/3_http_inteface_call.lua >> Pass
+  10. perl/1_string_match.pl >> Pass
+  11. perl/2_webpage_extract.pl >> Pass
+  12. perl/3_http_inteface_call.pl >> Pass
+  13. php/1_string_match.php >> Pass
+  14. php/2_webpage_extract.php >> Pass
+  15. php/3_http_inteface_call.php >> Pass
+  16. python/1_string_match.py >> Pass
+  17. python/2_webpage_extract.py >> Pass
+  18. python/3_http_inteface_call.py >> Pass
+  19. ruby/1_string_match.rb >> Pass
+  20. ruby/2_webpage_extract.rb >> Pass
+  21. ruby/3_http_inteface_call.rb >> Pass
+  22. tcl/1_string_match.tl >> Pass
+  23. tcl/2_webpage_extract.tl >> Pass
+  24. tcl/3_http_inteface_call.tl >> Pass
+  25. sample/1_simple.php >> Pass
+  26. sample/2_with_group.php >> Pass
+  27. sample/3_step_multi_lines.php >> Pass
+  28. sample/4_expect_saved_to_file.php >> Pass
+  29. sample/5_expect_with_express.php >> Pass
+  30. sample/6_expect_with_regx.php >> Pass
+  31. sample/7_expect_with_format_string.php >> Pass
+  32. sample/8_misc.php >> Pass
+  33. sample/9_skip.php >> Skip
 
 [esac]
 :start
 
-REM 此处编写代码，读取log，输出内容到控制台，ztf框架回同上述步骤进行验证
+for /f "delims="  eol^=^( %%i in (log/001/result.txt) do (
+    set "a=%%i"
+    echo ^>^> !a!
+)
--- demo/ztf/demo_test.sh
+++ demo/ztf/demo_test.sh
@@ -2,47 +2,49 @@
 
 :<<!
 [case]
-
 title=test demo scrtips
 cid=0
 pid=0
 
 [group]
-javascript/1_string_match.js             >> Pass
-javascript/2_webpage_extract.js          >> Pass
-javascript/3_http_inteface_call.js       >> Pass
-lua/1_string_match.lua                   >> Pass
-lua/2_webpage_extract.lua                >> Pass
-lua/3_http_inteface_call.lua             >> Pass
-perl/1_string_match.pl                   >> Pass
-perl/2_webpage_extract.pl                >> Pass
-perl/3_http_inteface_call.pl             >> Pass
-php/1_string_match.php                   >> Pass
-php/2_webpage_extract.php                >> Pass
-php/3_http_inteface_call.php             >> Pass
-python/1_string_match.py                 >> Pass
-python/2_webpage_extract.py              >> Pass
-python/3_http_inteface_call.py           >> Pass
-ruby/1_string_match.rb                   >> Pass
-ruby/2_webpage_extract.rb                >> Pass
-ruby/3_http_inteface_call.rb             >> Pass
-shell/1_string_match.sh                  >> Pass
-shell/2_webpage_extract.sh               >> Pass
-shell/3_http_inteface_call.sh            >> Pass
-tcl/1_string_match.tl                    >> Pass
-tcl/2_webpage_extract.tl                 >> Pass
-tcl/3_http_inteface_call.tl              >> Pass
-sample/1_simple.php                      >> Pass
-sample/2_with_group.php                  >> Pass
-sample/3_step_multi_lines.php            >> Pass
-sample/4_expect_saved_to_file.php        >> Pass
-sample/5_expect_with_express.php         >> Pass
-sample/6_expect_with_regx.php            >> Pass
-sample/7_expect_with_format_string.php   >> Pass
-sample/8_misc.php                        >> Pass
-sample/9_skip.php                        >> Skip
+  1. javascript/1_string_match.js >> Pass
+  2. javascript/2_webpage_extract.js >> Pass
+  3. javascript/3_http_inteface_call.js >> Pass
+  4. lua/1_string_match.lua >> Pass
+  5. lua/2_webpage_extract.lua >> Pass
+  6. lua/3_http_inteface_call.lua >> Pass
+  7. perl/1_string_match.pl >> Pass
+  8. perl/2_webpage_extract.pl >> Pass
+  9. perl/3_http_inteface_call.pl >> Pass
+  10. php/1_string_match.php >> Pass
+  11. php/2_webpage_extract.php >> Pass
+  12. php/3_http_inteface_call.php >> Pass
+  13. python/1_string_match.py >> Pass
+  14. python/2_webpage_extract.py >> Pass
+  15. python/3_http_inteface_call.py >> Pass
+  16. ruby/1_string_match.rb >> Pass
+  17. ruby/2_webpage_extract.rb >> Pass
+  18. ruby/3_http_inteface_call.rb >> Pass
+  19. shell/1_string_match.sh >> Pass
+  20. shell/2_webpage_extract.sh >> Pass
+  21. shell/3_http_inteface_call.sh >> Pass
+  22. tcl/1_string_match.tl >> Pass
+  23. tcl/2_webpage_extract.tl >> Pass
+  24. tcl/3_http_inteface_call.tl >> Pass
+  25. sample/1_simple.php >> Pass
+  26. sample/2_with_group.php >> Pass
+  27. sample/3_step_multi_lines.php >> Pass
+  28. sample/4_expect_saved_to_file.php >> Pass
+  29. sample/5_expect_with_express.php >> Pass
+  30. sample/6_expect_with_regx.php >> Pass
+  31. sample/7_expect_with_format_string.php >> Pass
+  32. sample/8_misc.php >> Pass
+  33. sample/9_skip.php >> Skip
 
 [esac]
 !
 
-# 此处编写代码，读取log，输出内容到控制台，ztf框架回同上述步骤进行验证
+cat log/001/result.txt | grep '^(' | while read line
+do
+   echo '>>' $line
+done
