a55423126386429a11af85b502faa3b74e7ed5f0
aaron <462826@qq.com>
2019-10-10 09:37:08 +0800

    use demo script to test

--- demo/ztf/demo_run.bat
+++ demo/ztf/demo_run.bat
@@ -1,18 +1,5 @@
 @echo off
-goto start
-[case]
-title=check string matches pattern
-cid=0
-pid=0
 
-[group]
-  1. exactly match >> hello
-  2. regular expression match >> 1[0-9]{10}
-  3. format string match >> %s%d
-
-[esac]
-:start
-
-echo ^>^> hello
-echo ^>^> 13905120512
-echo ^>^> abc123
+rm -rf log/*
+ztf.exe run demo/lang demo/sample
+ztf.exe run demo/ztf/demo_test.bat
