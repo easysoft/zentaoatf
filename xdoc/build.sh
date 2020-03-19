b71947fdaded9927f12d3bda0e36f391024ce4b4
aaron <462826@qq.com>
2020-03-19 14:02:31 +0800

    add win32 version; use tar.gz format for linux version

--- xdoc/build.sh
+++ xdoc/build.sh
@@ -6,21 +6,28 @@ cp -r demo ztf/
 
 go-bindata -o=res/res.go -pkg=res res/ res/doc res/json res/template
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ztf/ztf.exe src/ztf.go
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o ztf/ztf.exe src/ztf-386.go
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o ztf/ztf-x86.exe src/ztf.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ztf/ztf-amd64.exe src/ztf.go
+
 GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ztf/ztf-linux src/ztf.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ztf/ztf-mac src/ztf.go
 
 cd ztf
 
-zip -r ztf-win-2.0.zip ztf.exe demo conf log
+cp ztf-x86.exe ztf.exe
+zip -r ztf-win-x86-2.1.zip ztf.exe demo conf log
+rm ztf.exe
+
+cp ztf-amd64.exe ztf.exe
+zip -r ztf-win-amd64-2.1.zip ztf.exe demo conf log
+rm ztf.exe
 
 cp ztf-linux ztf
-zip -r ztf-linux-2.0.zip ztf demo conf log
+tar -zcvf ztf-linux-2.1.tar.gz ztf demo conf log
 rm ztf
 
 cp ztf-mac ztf
-zip -r ztf-mac-2.0.zip ztf demo conf log
+zip -r ztf-mac-2.1.zip ztf demo conf log
 rm ztf
 
 cd ..
