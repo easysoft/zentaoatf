7438d41766fd7bb4ddb27ff558a5072453144b1a
aaron <462826@qq.com>
2020-03-24 16:14:11 +0800

    set time related fields for report

--- src/action/unittest.go
+++ src/action/unittest.go
@@ -4,14 +4,21 @@ import (
 	testingService "github.com/easysoft/zentaoatf/src/service/testing"
 	zentaoService "github.com/easysoft/zentaoatf/src/service/zentao"
 	shellUtils "github.com/easysoft/zentaoatf/src/utils/shell"
+	"time"
 )
 
 func UnitTest(cmdStr string) {
+	startTime := time.Now().Unix()
 	shellUtils.ExeShellWithOutput(cmdStr)
+	endTime := time.Now().Unix()
 
 	testSuites := testingService.RetriveResult()
 	cases, classNameMaxWidth := testingService.ParserUnitTestResult(testSuites)
 
 	report := testingService.GenUnitTestReport(cases, classNameMaxWidth)
+
+	report.StartTime = startTime
+	report.EndTime = endTime
+	report.Duration = endTime - startTime
 	zentaoService.CommitUnitTestResult(report)
 }
