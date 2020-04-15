459f810bfab1d03631e6273d107e49f2a769d3cb
aaron <462826@qq.com>
2020-04-15 15:39:57 +0800

    remove unused mock method

--- xdoc/build.sh
+++ xdoc/build.sh
@@ -4,7 +4,7 @@ mkdir ztf/log
 cp -r conf ztf/
 cp -r demo ztf/
 
-go-bindata -o=res/res.go -pkg=res res/ res/doc res/json res/template
+/Users/aaron/go/bin/go-bindata -o=res/res.go -pkg=res res/ res/doc res/json res/template
 
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o ztf/ztf-x86.exe src/ztf.go
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ztf/ztf-amd64.exe src/ztf.go
