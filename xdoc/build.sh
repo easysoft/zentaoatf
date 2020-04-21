5468c53390c5213fe5e53a0d41f071e89db8d5fd
aaron <462826@qq.com>
2020-04-21 10:37:51 +0800

    update publish script

--- xdoc/build.sh
+++ xdoc/build.sh
@@ -16,19 +16,19 @@ CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ztf/ztf-mac src/ztf.go
 cd ztf
 
 cp ztf-x86.exe ztf.exe
-zip -r ztf-win-x86-2.1.zip ztf.exe conf demo runtime log
+zip -r ztf-win-x86-2.1.zip ztf.exe conf demo runtime log -x runtime/selenium/chrome80
 rm ztf.exe
 
 cp ztf-amd64.exe ztf.exe
-zip -r ztf-win-amd64-2.1.zip ztf.exe conf demo runtime log
+zip -r ztf-win-amd64-2.1.zip ztf.exe conf demo runtime -x runtime/selenium/chrome80
 rm ztf.exe
 
 cp ztf-linux ztf
-tar -zcvf ztf-linux-2.1.tar.gz ztf conf demo runtime log
+tar --exclude=runtime/php --exclude=runtime/selenium/chrome80 --exclude=runtime/selenium/chrome80.exe -zcvf ztf-linux-2.1.tar.gz ztf conf demo runtime log
 rm ztf
 
 cp ztf-mac ztf
-zip -r ztf-mac-2.1.zip ztf conf demo runtime log
+zip -r ztf-mac-2.1.zip ztf conf demo runtime log -x "runtime/php*" -x "runtime/selenium/chrome80" -x "runtime/selenium/chrome80.exe"
 rm ztf
 
 cd ..
