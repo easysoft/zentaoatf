d8a9bc3390d7ad66f5c87d99ff16548e999265c2
aaron <462826@qq.com>
2020-12-15 16:47:33 +0800

    computer the time if framework not support timing

--- src/action/unitTest.go
+++ src/action/unitTest.go
@@ -4,13 +4,20 @@ import (
 	testingService "github.com/easysoft/zentaoatf/src/service/testing"
 	zentaoService "github.com/easysoft/zentaoatf/src/service/zentao"
 	shellUtils "github.com/easysoft/zentaoatf/src/utils/shell"
+	time2 "time"
 )
 
 func RunUnitTest(cmdStr string) {
+	startTime := time2.Now().Unix()
 	shellUtils.ExeShellWithOutput(cmdStr)
+	endTime := time2.Now().Unix()
 
 	testSuites := testingService.RetrieveUnitResult()
 	cases, classNameMaxWidth, time := testingService.ParserUnitTestResult(testSuites)
+
+	if time == 0 {
+		time = float32(endTime - startTime)
+	}
 
 	report := testingService.GenUnitTestReport(cases, classNameMaxWidth, time)
 
