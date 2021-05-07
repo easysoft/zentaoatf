c5e9ef3029796a186e1ac2e7accaeeb7350e4afc
aaron <462826@qq.com>
2021-05-07 17:09:19 +0800

    update bat scripts

--- demo/lang/bat/2_webpage_extract.bat
+++ demo/lang/bat/2_webpage_extract.bat
@@ -1,23 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 goto start
-[case]
+
 title=extract content from webpage
 cid=0
 pid=0
 
-[group]
-  1. Load web page from url http://xxx 
-  2. Retrieve img element zt-logo.png in html 
-  3. Check img exist >> `.*zt-logo.png`
+  1. Load web page from url http://xxx
+  2. Retrieve img element zt-logo.png in html
+  3. Check img exist >> `.*.png`
 
-[esac]
 :start
 
-for /f "delims=" %%a in ('curl -s  "http://pms.zentao.net/user-login.html" ^| findstr/irc:"<img src="') do (
+for /f "delims=" %%a in ('curl -s  "http://max.demo.zentao.net/user-login-Lw==.html" ^| findstr/irc:"<img src="') do (
     set var=%%a
 )
-for /f "tokens=2 delims='" %%i in ("!var!") do (
+
+for /f "tokens=1 delims='" %%i in ("!var!") do (
     set var2=%%~i
 )
-echo ^>^> !var2!
+echo !var2!
--- demo/lang/bat/3_http_inteface_call.bat
+++ demo/lang/bat/3_http_inteface_call.bat
@@ -1,20 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 goto start
-[case]
+
 title=check remote interface response
 cid=0
 pid=0
 
-[group]
-  1. Send a request to interface http://xxx 
-  2. Retrieve sessionID field from response json 
-  3. Validate its format >> `^[a-z0-9]{26}`
+1. Send a request to interface http://xxx
+2. Retrieve sessionID field from response json
+3. Validate its format >> `^[a-z0-9]{26}`
 
-[esac]
 :start
 
-for /f "tokens=9 delims=," %%a in ('curl -s  "http://pms.zentao.net/?mode=getconfig"') do (
+for /f "tokens=9 delims=," %%a in ('curl -s  "http://max.demo.zentao.net/pms/?mode=getconfig"') do (
     set var=%%a
 )
 
@@ -23,4 +21,4 @@ for /f "tokens=2 delims=:" %%i in ("!var!") do (
     set var2=!s:~1,26!
 )
 
-echo ^>^> !var2!
+echo !var2!
