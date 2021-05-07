bb228ef5b45356d3377f1b41813375e613e429a1
aaron <462826@qq.com>
2021-05-07 16:17:30 +0800

    update php scripts

--- demo/lang/php/1_string_match.php
+++ demo/lang/php/1_string_match.php
@@ -1,19 +1,17 @@
 #!/usr/bin/env php
 <?php
 /**
-[case]
+
 title=check string matches pattern
 cid=0
 pid=0
 
-[group]
-  1. exactly match >> hello
-  2. regular expression match >> `1\d{10}`
-  3. format string match >> `%s%d`
+1. exactly match >> hello
+2. regular expression match >> `1\d{10}`
+3. format string match >> `%s%d`
 
-[esac]
 */
 
-print(">> hello\n");
-print(">> 13905120512\n");
-print(">> abc123\n");
+print("hello\n");
+print("13905120512\n");
+print("abc123\n");
--- demo/lang/php/3_http_inteface_call.php
+++ demo/lang/php/3_http_inteface_call.php
@@ -1,19 +1,17 @@
 #!/usr/bin/env php
 <?php
 /**
-[case]
+
 title=check remote interface response
 cid=0
 pid=0
 
-[group]
-  1. Send a request to interface http://xxx 
-  2. Retrieve sessionID field from response json 
-  3. Check its format >> `^[a-z0-9]{26}`
+1. Send a request to interface http://xxx
+2. Retrieve sessionID field from response json
+3. Check its format >> `^[a-z0-9]{26}`
 
-[esac]
 */
 
-$resp = file_get_contents('http://zentaopms.ngtesting.com//?mode=getconfig');
+$resp = file_get_contents('http://max.demo.zentao.net/pms/?mode=getconfig');
 $json = json_decode($resp);
-echo ">> " . $json->sessionID . "\n";
+echo $json->sessionID . "\n";
