7d1e671d5f25828d04f34e321d99d5cfae3b926b
aaron <462826@qq.com>
2021-06-16 17:34:41 +0800

    fix issues

--- demo/sample/8_extract_desc.php
+++ demo/sample/8_extract_desc.php
@@ -12,7 +12,7 @@ pid=0
 group2
   2.1 >> expect 2.1
   2.2 >> expect 2.2
-  2.3 >> expect 2.3  
+  2.3 >> expect 2.3
 
 multi line expect >>
   expect 3.1
@@ -23,7 +23,6 @@ multi line expect >>
 5 >> expect 5
 
 */
-
 
 // Step: 1    >> expect 1
 print("expect 1\n");
