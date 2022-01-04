ba62ba63071711c7a0e3641a26b2b32f21475475
aaron <462826@qq.com>
2022-01-04 17:10:28 +0800

    work dir selection

--- ui/src/layouts/IndexLayout/components/RightTopProject.vue
+++ ui/src/layouts/IndexLayout/components/RightTopProject.vue
@@ -1,17 +1,17 @@
 <template>
-    <a-select
-        ref="select"
-        v-model:value="currProject.id"
-        :bordered="true"
-        style="width: 160px"
-        @change="selectProject"
-    >
-      <a-select-option v-for="item in projects" :key="item.id" :value="item.id">{{ item.name }}</a-select-option>
-    </a-select>
+  <a-select
+      ref="select"
+      v-model:value="currProject.id"
+      :bordered="true"
+      style="width: 160px"
+      @change="selectProject"
+  >
+    <a-select-option v-for="item in projects" :key="item.id" :value="item.id">{{ item.name }}</a-select-option>
+  </a-select>
 </template>
 <script lang="ts">
-import { computed, ComputedRef, defineComponent, onMounted } from "vue";
-import { useStore } from "vuex";
+import {computed, ComputedRef, defineComponent, onMounted} from "vue";
+import {useStore} from "vuex";
 
 import {ProjectData} from "@/store/project";
 
@@ -23,30 +23,29 @@ interface RightTopProject {
 }
 
 export default defineComponent({
-    name: 'RightTopProjectSelection',
-    components: {
-    },
-    setup(): RightTopProject {
-      const store = useStore<{project: ProjectData}>();
+  name: 'RightTopProjectSelection',
+  components: {},
+  setup(): RightTopProject {
+    const store = useStore<{ project: ProjectData }>();
 
-      const projects = computed<any[]>(()=> store.state.project.projects);
-      const currProject = computed<any>(()=> store.state.project.currProject);
+    const projects = computed<any[]>(() => store.state.project.projects);
+    const currProject = computed<any>(() => store.state.project.currProject);
 
-      store.dispatch('project/fetchProject');
+    store.dispatch('project/fetchProject');
 
-      onMounted(()=>{
-        console.log('onMounted')
-      })
+    onMounted(() => {
+      console.log('onMounted')
+    })
 
-      const selectProject = (): void => {
-        console.log('selectProject')
-      }
-
-        return {
-          selectProject,
-          projects,
-          currProject
-        }
+    const selectProject = (): void => {
+      console.log('selectProject')
     }
+
+    return {
+      selectProject,
+      projects,
+      currProject
+    }
+  }
 })
 </script>
