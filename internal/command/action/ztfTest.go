a670369a8eb424f59253a8a2b49cdc01d3f04527
hind3ight <1157595693@qq.com>
2022-02-16 17:05:12 +0800

    run by files

--- internal/command/action/ztfTest.go
+++ internal/command/action/ztfTest.go
@@ -7,13 +7,16 @@ import (
 	zentaoUtils "github.com/aaronchen2k/deeptest/internal/comm/helper/zentao"
 	"github.com/aaronchen2k/deeptest/internal/command"
 	commandConfig "github.com/aaronchen2k/deeptest/internal/command/config"
+	"github.com/aaronchen2k/deeptest/internal/pkg/consts"
+	fileUtils "github.com/aaronchen2k/deeptest/internal/pkg/lib/file"
 	stringUtils "github.com/aaronchen2k/deeptest/internal/pkg/lib/string"
 	serverDomain "github.com/aaronchen2k/deeptest/internal/server/modules/v1/domain"
 	"github.com/kataras/iris/v12/websocket"
+	"path"
 	"strconv"
 )
 
-func RunZTFTest(file []string, suiteIdStr, taskIdStr string, actionModule *command.IndexModule) error {
+func RunZTFTest(files []string, suiteIdStr, taskIdStr string, actionModule *command.IndexModule) error {
 	cases := make([]string, 0)
 	req := serverDomain.WsReq{
 		ProjectPath: commConsts.WorkDir,
@@ -23,20 +26,37 @@ func RunZTFTest(file []string, suiteIdStr, taskIdStr string, actionModule *comma
 	if suiteIdStr != "" { // run with suite id
 		req.SuiteId = suiteIdStr
 		req.Act = commConsts.ExecSuite
-		cases = getCaseBySuiteId(suiteIdStr, file[0])
+		cases = getCaseBySuiteId(suiteIdStr, files[0])
 	} else if taskIdStr != "" { // run with task id,
 		req.TaskId = taskIdStr
 		req.Act = commConsts.ExecTask
 
 	} else {
+		suite, dir := isRunWithSuiteFile(files)
+		result := isRunWithResultFile(files)
 		req.Act = commConsts.ExecCase
+		if suite != "" { // run from suite file
+			if dir == "" { // not found dir in files param
+				dir = fileUtils.AbsolutePath(".")
+			}
+
+			//cases = getCaseBySuiteFile(suite, dir)
+		} else if result != "" { // run from failed result file
+			//cases = assertUtils.GetFailedCasesDirectlyFromTestResult(result)
+		} else { // run files
+			for _, v1 := range files {
+				if fileUtils.IsDir(v1) {
+					temps := scriptUtils.LoadScriptByProject(v1)
+					for _, v2 := range temps {
+						cases = append(cases, v2)
+					}
+				} else {
+					cases = append(cases, v1)
+				}
+			}
+		}
+
 	}
-	//if !fileUtils.IsDir(file[0]) {
-	//	cases = append(cases, file[0])
-	//} else {
-	//	_, _, _, scriptTree, _ := actionModule.ProjectService.GetByUser(commConsts.WorkDir)
-	//	cases = getCasesFromChildren(scriptTree.Children)
-	//}
 
 	req.Cases = cases
 
@@ -68,4 +88,36 @@ func getCaseBySuiteId(id string, dir string) []string {
 	}
 	scriptUtils.GetScriptByIdsInDir(dir, caseIdMap, &cases)
 	return cases
+}
+
+func isRunWithSuiteFile(files []string) (suiteFile, dir string) {
+	for _, file := range files {
+		if path.Ext(file) == "."+consts.ExtNameSuite {
+			suiteFile = file
+		} else {
+			if dir == "" { // only select the first dir
+				dir = file
+			}
+		}
+
+		if suiteFile != "" && dir != "" {
+			break
+		}
+	}
+
+	return
+}
+
+func isRunWithResultFile(files []string) string {
+	var resultFile string
+
+	for _, file := range files {
+		if path.Ext(file) == "."+consts.ExtNameResult || path.Ext(file) == "."+consts.ExtNameJson {
+			resultFile = file
+
+			return resultFile
+		}
+	}
+
+	return ""
 }
