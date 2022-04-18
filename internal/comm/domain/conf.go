826de9cc437f9b12193e82e7957c8ba6c822e03a
aaron <462826@qq.com>
2022-04-18 10:58:11 +0800

    fix issues

--- internal/comm/domain/conf.go
+++ internal/comm/domain/conf.go
@@ -6,14 +6,14 @@ type WorkspaceConf struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 
-	Javascript string `ini:"omitthis,omitempty" json:"javascript"`
-	Lua        string `ini:"omitthis,omitempty" json:"lua"`
-	Perl       string `ini:"omitthis,omitempty" json:"perl"`
-	Php        string `ini:"omitthis,omitempty" json:"php"`
-	Python     string `ini:"omitthis,omitempty" json:"python"`
-	Ruby       string `ini:"omitthis,omitempty" json:"ruby"`
-	Tcl        string `ini:"omitthis,omitempty" json:"tcl"`
-	Autoit     string `ini:"omitthis,omitempty" json:"autoit"`
+	Javascript string `ini:"omitempty" json:"javascript"`
+	Lua        string `ini:"omitempty" json:"lua"`
+	Perl       string `ini:"omitempty" json:"perl"`
+	Php        string `ini:"omitempty" json:"php"`
+	Python     string `ini:"omitempty" json:"python"`
+	Ruby       string `ini:"omitempty" json:"ruby"`
+	Tcl        string `ini:"omitempty" json:"tcl"`
+	Autoit     string `ini:"omitempty" json:"autoit"`
 
 	Version string `json:"version"`
 	IsWin   bool   `ini:"-" json:"isWin"`
