869809f474abe29be2ef38ea2d80b0c562143de9
Hao Sun <sunhao@easycorp.ltd>
2022-05-10 11:26:48 +0800

    + add List and ListItem components.

---
+++ ui/src/layouts/MainLayout/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="list" :class="{compact, divider}">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import ListItem, {ListItemProps} from './ListItem.vue';
+
+defineProps<{
+    compact?: boolean,
+    divider?: boolean,
+    items?: ListItemProps[]
+}>();
+
+</script>
