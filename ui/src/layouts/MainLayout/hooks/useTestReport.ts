e5f6719b5c7677495e66f009da455b13787b30bd
Hao Sun <sunhao@easycorp.ltd>
2022-05-31 15:19:33 +0800

    + add hook to use test report.

---
+++ ui/src/layouts/MainLayout/hooks/useTestReport.ts
@@ -0,0 +1,21 @@
+import {onMounted, watch, shallowRef, ShallowRef} from 'vue';
+import {useStore} from 'vuex';
+import {StateType} from '@/views/result/store';
+
+export default function useTestReport(seq: string, workspaceId: number): ShallowRef {
+    const store = useStore<{ Result: StateType }>();
+    const reportRef = shallowRef();
+
+    watch(() => store.state.Result.detailResult, (report) => {
+        if (!report || seq !== report.seq || workspaceId !== report.workspaceId) {
+            return;
+        }
+        reportRef.value = report;
+    }, {deep: true});
+
+    onMounted(() => {
+        store.dispatch('Result/get', {workspaceId: workspaceId, seq: seq});
+    });
+
+    return reportRef;
+}
