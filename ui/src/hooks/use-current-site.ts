108abb7a18ef3bc1d20c53324a93b7fa5fffe893
Hao Sun <sunhao@easycorp.ltd>
2022-06-06 12:58:53 +0800

    + add hook to use current size.

---
+++ ui/src/hooks/use-current-site.ts
@@ -0,0 +1,25 @@
+import { computed, ComputedRef } from 'vue';
+import { useStore } from 'vuex';
+import {ZentaoData} from '@/store/zentao';
+
+interface SiteInfo {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    deleted: boolean;
+    enabled: boolean;
+    isDefault: boolean;
+    name: string;
+    username: string;
+    url: string;
+}
+
+export default function useCurrentSite(): ComputedRef<SiteInfo> {
+    const store = useStore<{ Zentao: ZentaoData }>();
+    const currSite = computed<any>(() => {
+        const site = store.state.Zentao.currSite;
+        return site.username ? site : null;
+    });
+
+    return currSite;
+}
