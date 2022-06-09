a84c5a137dbd6746369c4690dcbb76ecc3a29af3
aaron <462826@qq.com>
2022-06-09 15:53:48 +0800

    fix issues

--- internal/pkg/lib/common/utils.go
+++ internal/pkg/lib/common/utils.go
@@ -93,7 +93,7 @@ func IsRelease() bool {
 	arg1 := strings.ToLower(os.Args[0])
 	name := filepath.Base(arg1)
 
-	return strings.Index(name, "___") != 0 && strings.Index(arg1, "go-build") < 0
+	return strings.Index(name, "___") != 0
 }
 
 func GetUserHome() string {
