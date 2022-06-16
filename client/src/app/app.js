d50acd9c2cbd469da4fbb823511f950e0cadb5aa
aaron <462826@qq.com>
2022-06-16 12:40:21 +0800

    open file in explore and terminal

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -207,7 +207,7 @@ export default class ZtfApp {
         if (IS_WINDOWS_OS) {
             cp.exec('start cmd.exe /K cd /D ' + path);
         } else if (IS_LINUX) {
-
+            cp.spawn ('/bin/bash', { cwd: path });
         } else if (IS_MAC_OSX) {
             cp.exec('open -a Terminal ' + path);
         }
