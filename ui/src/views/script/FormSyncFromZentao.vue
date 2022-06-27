7f3329fd789109acd9b0530b8948dc2144256e95
aaron <462826@qq.com>
2022-06-27 10:46:48 +0800

    fix issues

--- ui/src/views/script/FormSyncFromZentao.vue
+++ ui/src/views/script/FormSyncFromZentao.vue
@@ -73,7 +73,6 @@ import FormItem from "@/components/FormItem.vue";
 import notification from "@/utils/notification";
 import { useForm } from "@/utils/form";
 import Switch from "@/components/Switch.vue";
-import { ZentaoData } from "@/store/zentao";
 
 export interface FormWorkspaceProps {
   show?: boolean;
@@ -135,7 +134,8 @@ const submit = () => {
 const clearFormData = () => {
   modelRef.value = {};
 };
-const store = useStore<{ Workspace: WorkspaceData, Zentao: ZentaoData }>();
+
+const store = useStore<{ Workspace: WorkspaceData }>();
 const currSite = computed<any>(() => store.state.Zentao.currSite);
 const currProduct = computed<any>(() => store.state.Zentao.currProduct);
 const langs = computed<any[]>(() => store.state.Zentao.langs);
@@ -143,7 +143,9 @@ const modules = computed<any[]>(() => store.state.Zentao.modules);
 const suites = computed<any[]>(() => store.state.Zentao.suites);
 const tasks = computed<any[]>(() => store.state.Zentao.tasks);
 const fetchData = () => {
-  if(currSite.value.id == undefined || currSite.value.id <= 1 || currProduct.value.id == undefined) return;
+  store.dispatch("WorkspaceOld/list", currProduct.value.id);
+  if(currSite.value.id == undefined || currSite.value.id <= 1
+      || currProduct.value.id == undefined || currProduct.value.id <= 0) return;
   store.dispatch("Zentao/fetchModules", {
     siteId: currSite.value.id,
     productId: currProduct.value.id,
