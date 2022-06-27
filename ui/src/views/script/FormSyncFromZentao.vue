f5bb1c977e3caed7c9fa1a97b87f035840067352
aaron <462826@qq.com>
2022-06-27 10:42:37 +0800

    fix empty zentao site error

--- ui/src/views/script/FormSyncFromZentao.vue
+++ ui/src/views/script/FormSyncFromZentao.vue
@@ -144,7 +144,9 @@ const suites = computed<any[]>(() => store.state.Zentao.suites);
 const tasks = computed<any[]>(() => store.state.Zentao.tasks);
 const fetchData = () => {
   store.dispatch("WorkspaceOld/list", currProduct.value.id);
-  if(currSite.value.id == undefined || currSite.value.id <= 1 || currProduct.value.id == undefined) return;
+  if(currSite.value.id == undefined || currSite.value.id <= 1
+      || currProduct.value.id == undefined || currProduct.value.id <= 0) return;
+
   store.dispatch("Zentao/fetchModules", {
     siteId: currSite.value.id,
     productId: currProduct.value.id,
