b7d05d0a181ae4b8309a345e600c37c8b8cb8970
aaron <462826@qq.com>
2022-08-29 14:10:11 +0800

    fix the issue of list cmd output msg

--- internal/command/action/list.go
+++ internal/command/action/list.go
@@ -46,7 +46,7 @@ func List(files []string, keywords string) {
 	width := strconv.Itoa(len(strconv.Itoa(total)))
 
 	logUtils.Info("\n" + time.Now().Format("2006-01-02 15:04:05") + " " +
-		i118Utils.Sprintf("found_scripts", total, commConsts.WorkDir))
+		i118Utils.Sprintf("found_scripts_no_ztf_dir", total, commConsts.WorkDir))
 
 	for idx, cs := range scriptArr {
 		path := cs.Path
