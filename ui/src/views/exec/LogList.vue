456a224a2acd7db1b6c64bd01060d7cffae7f8f6
雨爱无痕 <yuaiwuhen@hotmail.com>
2022-09-14 14:10:19 +0800

    Fix issue

--- ui/src/views/exec/LogList.vue
+++ ui/src/views/exec/LogList.vue
@@ -294,7 +294,7 @@ const selectProxy = async (workspaceId, msg) => {
     workspaceInfo = await getWorkspace(workspaceId)
   }
   let selectedProxy = {data:{path:''}} as any;
-  if(workspaceInfo.data == undefined || workspaceInfo.data.proxies == ''){
+  if(workspaceInfo.data == undefined || workspaceInfo.data.proxies == '' || workspaceInfo.data.proxies == '0'){
     selectedProxy = {data:defaultProxy.value}
   }else{
     const msgSelectProxy = {
