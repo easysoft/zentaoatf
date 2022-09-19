87e770fd73c391fd8591186eccf857d3f3514ce6
雨爱无痕 <yuaiwuhen@hotmail.com>
2022-09-19 14:11:08 +0800

    save

--- test/ui_run_test.go
+++ test/ui_run_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"runtime"
 	"strconv"
 	"strings"
 	"testing"
@@ -1188,15 +1189,129 @@ func RunWorkspace(t provider.T) {
 	}
 }
 
+func RunUnit(t provider.T) {
+	testngDir := "./demo/ci_test_testng"
+	if runtime.GOOS == "windows" {
+		testngDir = ".\\demo\\ci_test_testng"
+	}
+	cloneGit("https://gitee.com/ngtesting/ci_test_testng.git", testngDir)
+	t.ID("5432")
+	t.Title("执行TestNG单元测试")
+	t.ID("5482")
+	t.AddParentSuite("右键执行脚本")
+	pw, err := playwright.Run()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+	headless := true
+	var slowMo float64 = 100
+	workspaceBrowser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{Headless: &headless, SlowMo: &slowMo})
+	if err != nil {
+		t.Errorf("Fail to launch the web workspaceBrowser: %v", err)
+		t.FailNow()
+	}
+	page, err := workspaceBrowser.NewPage()
+	if err != nil {
+		t.Errorf("Create the new page fail: %v", err)
+		t.FailNow()
+	}
+	if _, err = page.Goto("http://127.0.0.1:8000/", playwright.PageGotoOptions{
+		WaitUntil: playwright.WaitUntilStateDomcontentloaded}); err != nil {
+		t.Errorf("The specific URL is missing: %v", err)
+		t.FailNow()
+	}
+	_, err = page.WaitForSelector(".tree-node")
+	if err != nil {
+		t.Errorf("Wait tree-node fail: %v", err)
+		t.FailNow()
+	}
+	locator, err := page.Locator(".tree-node", playwright.PageLocatorOptions{HasText: "单元测试工作目录"})
+	c, err := locator.Count()
+	if err != nil || c == 0 {
+		t.Errorf("Find workspace fail: %v", err)
+		t.FailNow()
+	}
+	err = locator.Click(playwright.PageClickOptions{Button: playwright.MouseButtonRight})
+	if err != nil {
+		t.Errorf("Right click workspace fail: %v", err)
+		t.FailNow()
+	}
+	err = page.Click(".tree-context-menu>>text=执行")
+	if err != nil {
+		t.Errorf("Click copy fail: %v", err)
+		t.FailNow()
+	}
+	_, err = page.WaitForSelector("#log-list>>.msg-span>>:has-text('执行3个用例，耗时')")
+	if err != nil {
+		t.Errorf("Wait exec workspace result fail: %v", err)
+		t.FailNow()
+	}
+	locator, err = page.Locator("#log-list>>code:has-text('执行3个用例，耗时')")
+	if err != nil {
+		t.Errorf("Find exec workspace log fail: %v", err)
+		t.FailNow()
+	}
+	innerText, err := locator.InnerText()
+	if err != nil {
+		t.Errorf("Find exec workspace result fail: %v", err)
+		t.FailNow()
+	}
+	if !strings.Contains(innerText, "2(66.0%) 通过，1(33.0%) 失败") {
+		t.Errorf("Exec workspace fail: %v", err)
+		t.FailNow()
+	}
+	resultTitleElement, err := page.QuerySelector("#rightPane .result-list-item .list-item-title")
+	if err != nil {
+		t.Errorf("Find log title in logPane fail: %v", err)
+		t.FailNow()
+	}
+	resultTitle, err := resultTitleElement.InnerText()
+	if err != nil || resultTitle != "单元测试工作目录(3)" {
+		t.Errorf("Find result in rightPane fail: %v", err)
+		t.FailNow()
+	}
+	timeElement, err := page.Locator("#log-list>>.case-item:has-text('3_http_interface_call')>>.time>>span")
+	if err != nil {
+		t.Errorf("Find log time element in logPane fail: %v", err)
+		t.FailNow()
+	}
+	logTime, err := timeElement.InnerText()
+	if err != nil {
+		t.Errorf("Find log time in logPane fail: %v", err)
+		t.FailNow()
+	}
+	resultTimeElement, err := page.QuerySelector("#rightPane .result-list-item .list-item-trailing-text")
+	if err != nil {
+		t.Errorf("Find log time in rightPane fail: %v", err)
+		t.FailNow()
+	}
+	resultTime, err := resultTimeElement.InnerText()
+	if err != nil || logTime[:5] != resultTime {
+		t.Errorf("Find result in rightPane fail: %v", err)
+		t.FailNow()
+	}
+
+	if err = workspaceBrowser.Close(); err != nil {
+		t.Errorf("The workspaceBrowser cannot be closed: %v", err)
+		t.FailNow()
+	}
+	if err = pw.Stop(); err != nil {
+		t.Errorf("The playwright cannot be stopped: %v", err)
+		t.FailNow()
+	}
+}
+
 func TestUiRun(t *testing.T) {
-	runner.Run(t, "客户端-执行单个脚本", RunScript)
-	runner.Run(t, "客户端-执行选中的脚本文件和文件夹", RunSelectedScripts)
-	runner.Run(t, "客户端-执行打开的脚本文件", RunOpenedAndLast)
-	runner.Run(t, "客户端-执行所有的脚本文件", RunAll)
-	runner.Run(t, "客户端-结果中重新执行所有脚本", RunReExecAllCase)
-	runner.Run(t, "客户端-结果中重新执行失败脚本", RunReExecFailCase)
-	runner.Run(t, "客户端-确认执行统计成功数据", RunSuccessStatistic)
-	runner.Run(t, "客户端-确认执行统计失败数据", RunFailStatistic)
-	runner.Run(t, "客户端-确认执行统计bug数据", RunBugStatistic)
-	runner.Run(t, "客户端-右键执行工作目录", RunWorkspace)
+	// runner.Run(t, "客户端-执行单个脚本", RunScript)
+	// runner.Run(t, "客户端-执行选中的脚本文件和文件夹", RunSelectedScripts)
+	// runner.Run(t, "客户端-执行打开的脚本文件", RunOpenedAndLast)
+	// runner.Run(t, "客户端-执行所有的脚本文件", RunAll)
+	// runner.Run(t, "客户端-结果中重新执行所有脚本", RunReExecAllCase)
+	// runner.Run(t, "客户端-结果中重新执行失败脚本", RunReExecFailCase)
+	// runner.Run(t, "客户端-确认执行统计成功数据", RunSuccessStatistic)
+	// runner.Run(t, "客户端-确认执行统计失败数据", RunFailStatistic)
+	// runner.Run(t, "客户端-确认执行统计bug数据", RunBugStatistic)
+	// runner.Run(t, "客户端-右键执行工作目录", RunWorkspace)
+	runner.Run(t, "客户端-右键执行工作目录", RunUnit)
 }
