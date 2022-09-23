c77cf44a6238765ebfc9dc08612dc491472f5c08
aaron <462826@qq.com>
2022-09-23 14:09:19 +0800

    fix issue about unit testing

--- internal/pkg/helper/unit/uint.go
+++ internal/pkg/helper/unit/uint.go
@@ -7,9 +7,7 @@ import (
 func GetUnitTools(args []string, startIndex int) {
 	str := args[startIndex]
 
-	if commConsts.UnitTestType == commConsts.UnitTestTypeAllure {
-		commConsts.UnitTestTool = commConsts.Allure
-	} else if str == commConsts.UnitTestToolMvn {
+	if str == commConsts.UnitTestToolMvn {
 		commConsts.UnitBuildTool = commConsts.Maven
 	} else if str == commConsts.UnitTestToolMocha {
 		commConsts.UnitBuildTool = commConsts.Mocha
