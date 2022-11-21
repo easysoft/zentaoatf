87f0ce7b73c24565983d4bd38c2c762e9d726210
雨爱无痕 <yuaiwuhen@hotmail.com>
2022-11-21 13:58:36 +0800

    Add test fail exit code

--- test/helper/common/test.go
+++ test/helper/common/test.go
@@ -1,18 +1,13 @@
 package commonTestHelper
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"io"
-	"os/exec"
+	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"time"
 
 	commDomain "github.com/easysoft/zentaoatf/internal/pkg/domain"
-	analysisHelper "github.com/easysoft/zentaoatf/internal/pkg/helper/analysis"
 	execHelper "github.com/easysoft/zentaoatf/internal/pkg/helper/exec"
 	zentaoHelper "github.com/easysoft/zentaoatf/internal/pkg/helper/zentao"
 
@@ -23,67 +18,23 @@ import (
 )
 
 func TestCli() (err error) {
-	cmdStr := fmt.Sprintf(`%sztf allure -allureReportDir ./test/cli/allure-results go test %stest/cli -v`, RootPath, RootPath)
-	var cmd *exec.Cmd
+	testPath := fmt.Sprintf(`%stest`, RootPath)
 	if runtime.GOOS == "windows" {
-		cmdStr = fmt.Sprintf(`%sztf.exe allure -allureReportDir .\test\cli\allure-results go test %stest\cli -v`, RootPath, RootPath)
-		cmd = exec.Command("cmd", "/C", cmdStr)
-	} else {
-		cmd = exec.Command("/bin/bash", "-c", cmdStr)
+		testPath = fmt.Sprintf(`%stest`, RootPath)
 	}
-	cmd.Dir = RootPath
-	fmt.Println(cmd.String())
+	req := serverDomain.TestSet{
+		WorkspacePath: testPath,
+		Cmd:           "go test ./cli -v",
+		TestTool:      commConsts.GoTest,
+	}
+	report := ExecUnit(req)
 
-	if cmd == nil {
-		err = errors.New("cmd is nil")
-		return
-	}
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return
-	}
-
-	err = cmd.Start()
-	if err != nil {
-		return
-	}
-	defer func() {
-		cmd.Process.Kill()
-		execHelper.KillProcessByUUID("cli_auto_test")
-	}()
-	reader1 := bufio.NewReader(stdout)
-	reportDir := ""
-	for {
-		line, err2 := reader1.ReadString('\n')
-		line = strings.TrimSpace(line)
-		fmt.Println(line)
-		lineRune := []rune(line)
-		if len(lineRune) >= 6 && string(lineRune[:6]) == "Report" {
-			reportDir = strings.TrimSpace(string(lineRune[6 : len(lineRune)-1]))
-			break
-		} else if len(lineRune) >= 6 && string(lineRune[:2]) == "报告" {
-			reportDir = strings.TrimSpace(string(lineRune[2 : len(lineRune)-1]))
-			break
-		}
-		if err2 != nil {
-			err = err2
-			return
-		}
-		if err != nil || io.EOF == err {
-			break
-		}
-	}
-	if reportDir == "" {
-		return
-	}
-
-	report, err := analysisHelper.ReadReportByPath(strings.Replace(reportDir, "result.txt", "result.json", 1))
-	if err != nil {
-		return
-	}
 	config := commDomain.WorkspaceConf{Url: "http://127.0.0.1:8081/", Password: "Test123456.", Username: "admin"}
-
 	err = zentaoHelper.CommitResult(report, 1, 0, config, nil)
+
+	if report.Fail > 0 {
+		os.Exit(1)
+	}
 	return
 }
 
@@ -100,8 +51,11 @@ func TestUi() (err error) {
 	report := ExecUnit(req)
 
 	config := commDomain.WorkspaceConf{Url: "http://127.0.0.1:8081/", Password: "Test123456.", Username: "admin"}
-
 	err = zentaoHelper.CommitResult(report, 1, 0, config, nil)
+
+	if report.Fail > 0 {
+		os.Exit(1)
+	}
 	return
 }
 
