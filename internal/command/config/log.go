3414d3f7b5c2e5dffe7b09e211df6c9252dcfcf9
zhaoke <zhaoke@easycorp.ltd>
2022-11-25 10:25:51 +0800

    * Fix cli test error

--- internal/command/config/log.go
+++ internal/command/config/log.go
@@ -1,17 +1,39 @@
 package commandConfig
 
 import (
+	"os"
+
 	logUtils "github.com/easysoft/zentaoatf/pkg/lib/log"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 func InitLog() {
-	cfg := zap.NewDevelopmentConfig()
-	cfg.EncoderConfig.CallerKey = zapcore.OmitKey
-	cfg.EncoderConfig.TimeKey = zapcore.OmitKey
-	cfg.EncoderConfig.LevelKey = zapcore.OmitKey
+	zapConfig := zap.Config{
+		Level:         zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development:   true,
+		Encoding:      "console",
+		EncoderConfig: zapcore.EncoderConfig{},
+	}
 
-	logUtils.LoggerStandard, _ = cfg.Build()
-	logUtils.LoggerExecConsole, _ = cfg.Build()
+	logUtils.LoggerStandard, _ = zapConfig.Build(zap.WrapCore(zapCoreConsole))
+	logUtils.LoggerExecConsole, _ = zapConfig.Build(zap.WrapCore(zapCoreConsole))
+}
+
+func zapCoreConsole(c zapcore.Core) zapcore.Core {
+	encoderConfig := zap.NewDevelopmentEncoderConfig()
+	encoderConfig.TimeKey = zapcore.OmitKey
+	encoderConfig.LevelKey = zapcore.OmitKey
+	encoderConfig.CallerKey = zapcore.OmitKey
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		zapcore.NewMultiWriteSyncer(
+			zapcore.AddSync(os.Stdout),
+		),
+		zap.DebugLevel,
+	)
+	cores := zapcore.NewTee(c, core)
+
+	return cores
 }
