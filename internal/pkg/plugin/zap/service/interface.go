2d857816d5da37bf9aeb31edefecc8956ebe77e5
aaron <462826@qq.com>
2023-05-26 10:40:10 +0800

    plugin communication sample

--- internal/pkg/plugin/zap/service/interface.go
+++ internal/pkg/plugin/zap/service/interface.go
@@ -1,6 +1,5 @@
 package zapService
 
-// ZapInterface is the interface that we're exposing as a plugin.
 type ZapInterface interface {
 	Put(key string, value []byte) error
 	Get(key string) ([]byte, error)
