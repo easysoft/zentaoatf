71307e3727017fae90921cedcb31ff114e98b683
aaron <462826@qq.com>
2023-05-30 11:16:02 +0800

    plugin communication sample

--- test/restapi/api_product_test.go
+++ test/restapi/api_product_test.go
@@ -3,17 +3,24 @@ package main
 import (
 	"github.com/ozontech/allure-go/pkg/framework/provider"
 	"github.com/ozontech/allure-go/pkg/framework/suite"
+	"testing"
 )
+
+func TestProductApi(t *testing.T) {
+	suite.RunSuite(t, new(ProductApiSuite))
+}
+
+type ProductApiSuite struct {
+	suite.Suite
+}
 
 func (s *ProductApiSuite) BeforeEach(t provider.T) {
 	t.ID("1")
 	t.AddSubSuite("ProductApi")
 }
 
-func (s *ProductApiSuite) TestMe(t provider.T) {
-	t.Require().Equal("Success", "Success")
-}
-
-type ProductApiSuite struct {
-	suite.Suite
+func (s *ProductApiSuite) TestProductApiTest(t provider.T) {
+	t.WithNewStep("My Second Step!", func(sCtx provider.StepCtx) {
+		sCtx.NewStep("My First SubStep!")
+	})
 }
