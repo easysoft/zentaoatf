a8842096621df8298d6ff69b0ad69736bac609ab
aaron <462826@qq.com>
2023-05-30 11:33:13 +0800

    plugin communication sample

--- test/restapi/api_product_test.go
+++ test/restapi/api_product_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	httpUtils "github.com/easysoft/zentaoatf/pkg/lib/http"
+	constTestHelper "github.com/easysoft/zentaoatf/test/helper/conf"
 	"github.com/ozontech/allure-go/pkg/framework/provider"
 	"github.com/ozontech/allure-go/pkg/framework/suite"
 	"testing"
@@ -19,7 +22,10 @@ func (s *ProductApiSuite) BeforeEach(t provider.T) {
 	t.AddSubSuite("ProductApi")
 }
 
-func (s *ProductApiSuite) TestProductApiTest(t provider.T) {
+func (s *ProductApiSuite) TestProductApi(t provider.T) {
+	url := fmt.Sprintf("http://127.0.0.1:%d/api.php/v1/products", constTestHelper.ZentaoPort)
+	httpUtils.Get(url)
+
 	t.WithNewStep("My Second Step!", func(sCtx provider.StepCtx) {
 		sCtx.NewStep("My First SubStep!")
 	})
