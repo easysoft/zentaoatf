4db651843b14743860727a274030a970c25c4eba
aaron <462826@qq.com>
2023-06-05 13:21:25 +0800

    move test scripts

--- test/helper/conf/conf.go
+++
@@ -1,5 +0,0 @@
-package constTestHelper
-
-func SetZentaoSiteUrl(url string) {
-	ZentaoSiteUrl = url
-}
--- test/ui/conf/variable.go
+++
@@ -1,9 +0,0 @@
-package conf
-
-var (
-	ExitAllOnError         = true
-	ShowErr                = true
-	Headless               = true
-	Timeout        float64 = 5000
-	Locale         string  = "zh-CN"
-)
